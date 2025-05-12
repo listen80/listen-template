ec083b0d33aabc0283897877e44bed4e78f23969
李嘉宾 <lijiabin11@byd.com>
2025-05-12 16:57:13 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -20,7 +20,8 @@ if (process.env.NODE_ENV === "development") {
   plugins.push(
     serve({
       port: process.env.PORT || 9000,
-      contentBase: "",
+      contentBase: "./",
+      historyApiFallback: '/README.md',
     })
   );
 } else {
